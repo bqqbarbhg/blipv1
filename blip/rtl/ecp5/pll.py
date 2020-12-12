22b3eee6a82a26744f65f3b8da5cdc67b401c523
bqqbarbhg <samuli.1995@hotmail.com>
2020-12-12 21:21:03 +0200

    Ecp5Pll: VCO -> {VCO}

--- blip/rtl/ecp5/pll.py
+++ blip/rtl/ecp5/pll.py
@@ -63,7 +63,7 @@ class Ecp5Pll(Elaboratable):
 
         The phase detector {PD} compares its two inputs, the *reference clock*
         `i_clk/ref_div` and the *feedback clock* `VCO/fb_div`, and adjusts the
-        frequency of the VCO until they are in sync. This causes VCO to run at
+        frequency of the {VCO} until they are in sync. This causes VCO to run at
         the frequency `i_clk * (fb_div/ref_div)`. The individual output clocks
         are obtained by dividing VCO by their respective output divisors.
 
